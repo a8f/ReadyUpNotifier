b44c1c5b470c02af1f46289b4f44d1fc117478d8
git-uname <a.french@mail.utoronto.ca>
2018-12-24 00:05:52 -0500

    Refactor server to its own class

--- pc/main.py
+++ pc/main.py
@@ -1,80 +1,8 @@
-import re
-from threading import Thread
-from time import sleep
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from server import Server
 
-# TODO find path depending on os or prompt for path
-#path = "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo"
-path = "."
-#ready_message_regex = r'Server reservation check (\w)* ready-up!'
-ready_message_regex = r'ready'
-cancelled_message_regex = r'cancel'
-queue_message_regex = r'queue'
-ip = "localhost"
-port = 1234
-update_rate = 1
-client = None
-connected = False
-done = False
-match_found = False
-in_queue = False
-
-def update_log():
-    global match_found, done, connected, client, in_queue
-    with open(path + "/console.log", "r") as f:
-        f.seek(0,2) # Go to end of file
-        while True:
-            line = f.readline()
-            if not line:
-                sleep(update_rate)
-                continue
-            if match_found and re.match(cancelled_message_regex, line):
-                match_found = False
-                client.sendMessage("cancel")
-                continue
-            if re.match(ready_message_regex, line):
-                match_found = True
-                client.sendMessage("ready")
-                continue
-            if re.match(queue_message_regex, line):
-                client.sendMessage("queue")
-                in_queue = True
-                continue
-
-class Server(WebSocket):
-    def handleMessage(self):
-        self.sendMessage(self.data)
-
-    def handleConnected(self):
-        global connected, client, match_found, in_queue
-        client = self
-        connected = True
-        print(self.address, " connected")
-        if match_found:
-            self.sendMessage("ready")
-        elif in_queue:
-            self.sendMessage("queue")
-        else:
-            self.sendMessage("waiting")
-
-    def handleClose(self):
-        global done
-        #done = True
-        print(self.address, " closed")
-
-def serve():
-    global done
-    while not done:
-        server.serveonce()
-
-if __name__ == '__main__':
-    server = SimpleWebSocketServer(ip, port, Server)
-    #Thread(target=server.serveforever).start()
-    server_thread = Thread(target=serve)
-    server_thread.start()
-    print("awaiting connection\n")
-    while not connected: pass
-    print("connected\n")
-    update_log()
-    server_thread.join()
-    print("done")
+s = Server(ip="localhost", port=1234)
+try:
+    s.start()
+except KeyboardInterrupt:
+    s.socket.shutdown()
+    raise SystemExit
---
+++ pc/server.py
@@ -0,0 +1,76 @@
+import re
+from threading import Thread
+from time import sleep
+from websocket_server import WebsocketServer
+
+#path = "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo"
+#ready_message_regex = r'Server reservation check (\w)* ready-up!'
+
+class Server():
+    connected = False
+    match_found = False
+    in_queue = False
+    done = False
+
+    def __init__(self, ready_regex=r'ready', cancel_regex=r'cancel', queue_regex=r'queue', ip='localhost', port=1234, update_rate=1, path='.', logfile='console.log'):
+        self.path = "."
+        self.ready_regex = ready_regex
+        self.cancel_regex = cancel_regex
+        self.queue_regex = queue_regex
+        self.ip = ip
+        self.port = port
+        self.update_rate = update_rate
+        self.path = path
+        self.logfile = logfile
+        self.socket = None
+
+    def update_log(self):
+        with open(self.path + "/console.log", "r") as f:
+            f.seek(0,2) # Go to end of file
+            while not self.done:
+                line = f.readline()
+                if not line:
+                    sleep(self.update_rate)
+                    continue
+                if self.match_found and re.match(self.cancelled_regex, line):
+                    Server.match_found = False
+                    self.socket.send_message_to_all("cancel")
+                    continue
+                if re.match(self.ready_regex, line):
+                    Server.match_found = True
+                    self.socket.send_message_to_all("ready")
+                    continue
+                if re.match(self.queue_regex, line):
+                    self.socket.send_message_to_all("queue")
+                    Server.in_queue = True
+                    continue
+
+    def serve(self):
+        '''
+        while not self.done:
+            self.socket.handle_request()
+        '''
+        self.socket.serve_forever()
+
+    def on_message(self, client, server, message):
+        self.socket.send_message_to_all(message)
+
+    def on_client_join(self, client, server):
+        print("Client {} connected".format(client["id"]))
+        self.connected = True
+        if self.match_found:
+            self.socket.send_message_to_all("ready")
+        elif self.in_queue:
+            self.socket.send_message_to_all("queue")
+        else:
+            self.socket.send_message_to_all("waiting")
+
+    def start(self):
+        self.socket = WebsocketServer(self.port, host=self.ip)
+        self.socket.set_fn_message_received(self.on_message)
+        self.socket.set_fn_new_client(self.on_client_join)
+        server_thread = Thread(target=self.serve)
+        server_thread.start()
+        print("Awaiting connection")
+        self.update_log()
+        server_thread.join()
